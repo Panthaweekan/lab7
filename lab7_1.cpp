466173d31941a2a2f52d56b5c2b567737a6c3ef9
Panthaweekan <tatanoobza1234@hotmail.com>
2021-11-30 17:46:32 +0700

    add finish lab7_1.cpp

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,27 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+
+	string text,ReverseText,TolowerText,ReverseTolowerText;
+
+    cout << "Input text: ";
+	cin >> text;
+	ReverseText = func1(text);
+	TolowerText = func2(text);
+	ReverseTolowerText = func2(ReverseText);
+    cout << "Reversed text: " << ReverseText << "\n";
+
+	if(text == ReverseText || TolowerText == ReverseTolowerText)
+	{
+		cout << "Palindrome: Yes";
+
+	}else{
+
+		cout << "Palindrome: No"; 
+	}
+	
     return 0;
+
+	// FINISH_640612093
+
 }
